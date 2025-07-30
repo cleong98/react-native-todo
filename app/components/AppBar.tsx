76118eb001b7d915885a4ae716e13f8e7dbe2f6b
cleong98 <zhuzhuloh1998@gmail.com>
2025-07-30 22:38:39 +0800

    chore: fix ios icon issue and add appBar configuration

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,26 +1,58 @@
 import React, { FC, isValidElement, ReactNode } from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from '@react-native-vector-icons/fontawesome6';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
 
 interface AppBarProps {
   title: string | ReactNode;
   action?: ReactNode;
+  trailing?: ReactNode;
+  hideBackButton?: boolean;
 }
 
-const AppBar: FC<AppBarProps> = ({title, action}) => {
+const AppBar: FC<AppBarProps> = ({
+  title,
+  action,
+  trailing,
+  hideBackButton = false,
+}) => {
+  const navigation = useNavigation();
+
+  const canGoBack = navigation.canGoBack();
+  const isShowBackIcon = !hideBackButton && canGoBack;
+
+  const onBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
-      
-       <View style={{flex: 1, alignItems: 'center'}}>
-        {
-           isValidElement(title) ? title : <Text style={styles.title}>{title}</Text>
-        }
+      {isShowBackIcon ? (
+        <View style={{ paddingLeft: 20 }}>
+          <TouchableOpacity onPress={onBack}>
+            <Icon size={18} name="arrow-left" iconStyle="solid" />
+          </TouchableOpacity>
         </View>
-      
-
+      ) : (
+        trailing ?? <View style={{ width: 40 }} />
+      )}
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        {isValidElement(title) ? (
+          title
+        ) : (
+          <Text style={styles.title}>{title}</Text>
+        )}
+      </View>
       {action}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -30,17 +62,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     ...Platform.select({
-      android:{
-         borderBottomWidth:0.5,
-         borderBottomColor:'#00000033'      
+      android: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#00000033',
       },
-      default:{
+      default: {
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 3.84,
         shadowOpacity: 0.25,
-      }
-    })
-
+      },
+    }),
   },
   title: {
     fontSize: 18,
--- app/features/todo/AddTodoScreen.tsx
+++ app/features/todo/AddTodoScreen.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import AppBar from '@app/components/AppBar';
+import Icon from '@react-native-vector-icons/fontawesome6';
+import IconButton from './components/IconButton';
+import { useNavigation } from '@react-navigation/native';
 
 const AddTodoScreen = () => {
-  return (
-    <View>
-      <Text>AddTodoScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default AddTodoScreen
+  return (
+    <SafeAreaView>
+      <AppBar
+        title="Add Todo"
+        action={
+          <IconButton
+            disabled={true}
+            icon={
+              <Icon size={16} name="trash" iconStyle="solid" color={'white'} />
+            }
+            onPress={() => {}}
+            backgroundColor="#F44336"
+          />
+        }
+      />
+      <Text>AddTodoScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default AddTodoScreen;
